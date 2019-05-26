ee552e850f95689d47f7448ce84d45eec894273a
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-26 06:56:30 +0000

    Deploy k-karen/vue-chartjs-sample to github.com/k-karen/vue-chartjs-sample.git:gh-pages

---
+++ precache-manifest.e18b97d1f5e4672459b8acc676d0befb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "adee7ffd3c8c4c933220",
+    "url": "/vue-chartjs-sample/js/app.e9c9d0cf.js"
+  },
+  {
+    "revision": "8e2217e0c2c2db497513",
+    "url": "/vue-chartjs-sample/css/chunk-vendors.1e18bcfd.css"
+  },
+  {
+    "revision": "8e2217e0c2c2db497513",
+    "url": "/vue-chartjs-sample/js/chunk-vendors.311bee55.js"
+  },
+  {
+    "revision": "3500d54db72f2e39379c647918b6fc9d",
+    "url": "/vue-chartjs-sample/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-chartjs-sample/robots.txt"
+  }
+];
